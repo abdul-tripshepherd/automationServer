6e4cb3c65760a59e13728b626f558f305b11e18c
Abdul <abdulmuhaymim@gmail.com>
2024-05-07 12:52:03 +0500

    unnamed

--- seleniumScripts/productPriceChecker.py
+++ seleniumScripts/productPriceChecker.py
@@ -51,6 +51,7 @@ class productChecker:
         adult_count = 0
         child_count = 0
         infant_count = 0
+        incorrect_prices = []
 
         self.driver.get("https://www.tripshepherd.com")
         time.sleep(5)
@@ -62,73 +63,85 @@ class productChecker:
 
         wait = WebDriverWait(self.driver, 10)
 
-        all_cities = wait.until(EC.visibility_of_element_located((By.XPATH, '//*[@id="hero"]/div[1]/button')))
-        all_cities.click()
         for j in range(1, 41):
-            city = wait.until(EC.visibility_of_element_located((By.XPATH, f'//*[@id="hero"]/div[2]/div/div[2]/a[{j}]')))
-            city.click()
-            for i in range(1, 10):
-                try:
-                    product = wait.until(EC.visibility_of_element_located((By.XPATH, f'//*[@id="feature_experiences_cards"]/div/a[{i}]')))
-                    match = re.search(r'\$([\d,]+)$', product.text.splitlines()[-1])
-                    if match:
-                        price = match.group(1)
-                        price = int(price.replace(',', ''))
-                        product_main_price = price
-                    product.click()
-
+            try:
+                all_cities = wait.until(EC.visibility_of_element_located((By.XPATH, '//*[@id="hero"]/div[1]/button')))
+                all_cities.click()
+                city = wait.until(EC.visibility_of_element_located((By.XPATH, f'//*[@id="hero"]/div[2]/div/div[2]/a[{j}]')))
+                city.click()
+                for i in range(1, 15):
+                    print(j)
                     try:
-                        popup = wait.until(EC.visibility_of_element_located((By.XPATH, '//*[@id="Judith Blake"]/button/svg'))).click()
+                        product = wait.until(EC.visibility_of_element_located((By.XPATH, f'//*[@id="feature_experiences_cards"]/div/a[{i}]')))
+                        match = re.search(r'\$([\d,]+)$', product.text.splitlines()[-1])
+                        if match:
+                            price = match.group(1)
+                            price = int(price.replace(',', ''))
+                            product_main_price = price
+                        product.click()
+
+                        try:
+                            popup = wait.until(EC.visibility_of_element_located((By.XPATH, '//*[@id="Judith Blake"]/button/svg'))).click()
+                        except:
+                            pass
+                            
+                        
+                        tour_name = wait.until(EC.visibility_of_element_located((By.XPATH, '//*[@id="__next"]/main/div[2]/div/div[2]/div[3]/div[1]/div[1]/h1'))).text
+
+                        starting_from_text = wait.until(EC.visibility_of_element_located((By.XPATH, '//*[@id="__next"]/main/div[2]/div/div[2]/div[2]/div/div/div[1]/div/div/p')))
+                        starting_from_price = extract_number(starting_from_text.text)
+                        
+                        calendar_expand = wait.until(EC.visibility_of_element_located((By.XPATH, '//*[@id="__next"]/main/div[2]/div/div[2]/div[2]/div/div/div[1]/div/div/div[1]/div/img')))
+                        calendar_expand.click()
+                        date_btn = wait.until(EC.visibility_of_element_located((By.XPATH, '//*[@id="__next"]/main/div[2]/div/div[2]/div[2]/div/div/div[1]/div/div/div[3]/div/div[2]/div/div/div/div[2]/button[18]/div')))
+                        calendar_price = extract_number(date_btn.text)
+                        date_btn.click()
+                        no_of_pax_expand = wait.until(EC.visibility_of_element_located((By.XPATH, '//*[@id="__next"]/main/div[2]/div/div[2]/div[2]/div/div/div[1]/div/div/div[3]/div/div[1]/button/img')))
+                        no_of_pax_expand.click()
+
+                        add_adult = wait.until(EC.visibility_of_element_located((By.XPATH, '//*[@id="__next"]/main/div[2]/div/div[2]/div[2]/div/div/div[1]/div/div/div[3]/div/div[2]/div[1]/div[2]/div[3]')))
+                        add_adult.click()
+                        add_adult_price = wait.until(EC.visibility_of_element_located((By.XPATH, '//*[@id="__next"]/main/div[2]/div/div[2]/div[2]/div/div/div[1]/div/div/div[3]/div/div[2]/div[1]/div[1]/div[2]/div/span')))
+                        adult_price = extract_number(add_adult_price.text)
+                        add_child = wait.until(EC.visibility_of_element_located((By.XPATH, '//*[@id="__next"]/main/div[2]/div/div[2]/div[2]/div/div/div[1]/div/div/div[3]/div/div[2]/div[2]/div[2]/div[3]')))
+                        add_child.click()
+                        add_child_price = wait.until(EC.visibility_of_element_located((By.XPATH, '//*[@id="__next"]/main/div[2]/div/div[2]/div[2]/div/div/div[1]/div/div/div[3]/div/div[2]/div[2]/div[1]/div[2]/div/span')))
+                        child_price = extract_number(add_child_price.text)
+                        add_infant = wait.until(EC.visibility_of_element_located((By.XPATH, '//*[@id="__next"]/main/div[2]/div/div[2]/div[2]/div/div/div[1]/div/div/div[3]/div/div[2]/div[3]/div[2]/div[3]')))
+                        add_infant.click()
+                        add_infant_price = wait.until(EC.visibility_of_element_located((By.XPATH, '//*[@id="__next"]/main/div[2]/div/div[2]/div[2]/div/div/div[1]/div/div/div[3]/div/div[2]/div[3]/div[1]/div[2]/div/span')))
+                        infant_price = extract_number(add_infant_price.text)
+
+                        book_now_button = wait.until(EC.visibility_of_element_located((By.XPATH, '//*[@id="__next"]/main/div[2]/div/div[2]/div[2]/div/div/div[1]/div/div/button')))
+                        book_now_price = extract_number(book_now_button.text)
+                        book_now_button.click()
+
+                        adult_price_2 = extract_number(wait.until(EC.visibility_of_element_located((By.XPATH,'//*[@id="__next"]/main/div[2]/div/div[2]/div[1]/div[1]/div[3]/div[1]/h2'))).text)
+                        child_price_2 = extract_number(wait.until(EC.visibility_of_element_located((By.XPATH,'//*[@id="__next"]/main/div[2]/div/div[2]/div[1]/div[1]/div[3]/div[2]/h2'))).text)
+                        infant_price_2 = extract_number(wait.until(EC.visibility_of_element_located((By.XPATH,'//*[@id="__next"]/main/div[2]/div/div[2]/div[1]/div[1]/div[3]/div[3]/h2'))).text)
+                        total_price = extract_number(wait.until(EC.visibility_of_element_located((By.XPATH,'//*[@id="__next"]/main/div[2]/div/div[2]/div[1]/div[1]/div[3]/div[4]/div[2]/h2'))).text)
+
+                        if total_price == book_now_price == infant_price + child_price + adult_price*3 == adult_price_2 + child_price_2 + infant_price_2 and starting_from_price == calendar_price == product_main_price:
+                            print(tour_name + ": Prices are correct")
+                        else:
+                            print(tour_name + ": Prices are incorrect")
+                            incorrect_prices.append({"name": tour_name, "city_number": j, "product_number": i})
+
+                        self.driver.back()
+                        self.driver.back()
                     except:
                         pass
-                        
-                    
-                    tour_name = wait.until(EC.visibility_of_element_located((By.XPATH, '//*[@id="__next"]/main/div[2]/div/div[2]/div[3]/div[1]/div[1]/h1'))).text
-
-                    starting_from_text = wait.until(EC.visibility_of_element_located((By.XPATH, '//*[@id="__next"]/main/div[2]/div/div[2]/div[2]/div/div/div[1]/div/div/p')))
-                    starting_from_price = extract_number(starting_from_text.text)
-                    
-                    calendar_expand = wait.until(EC.visibility_of_element_located((By.XPATH, '//*[@id="__next"]/main/div[2]/div/div[2]/div[2]/div/div/div[1]/div/div/div[1]/div/img')))
-                    calendar_expand.click()
-                    date_btn = wait.until(EC.visibility_of_element_located((By.XPATH, '//*[@id="__next"]/main/div[2]/div/div[2]/div[2]/div/div/div[1]/div/div/div[3]/div/div[2]/div/div/div/div[2]/button[18]/div')))
-                    calendar_price = extract_number(date_btn.text)
-                    date_btn.click()
-                    no_of_pax_expand = wait.until(EC.visibility_of_element_located((By.XPATH, '//*[@id="__next"]/main/div[2]/div/div[2]/div[2]/div/div/div[1]/div/div/div[3]/div/div[1]/button/img')))
-                    no_of_pax_expand.click()
-
-                    add_adult = wait.until(EC.visibility_of_element_located((By.XPATH, '//*[@id="__next"]/main/div[2]/div/div[2]/div[2]/div/div/div[1]/div/div/div[3]/div/div[2]/div[1]/div[2]/div[3]')))
-                    add_adult.click()
-                    add_adult_price = wait.until(EC.visibility_of_element_located((By.XPATH, '//*[@id="__next"]/main/div[2]/div/div[2]/div[2]/div/div/div[1]/div/div/div[3]/div/div[2]/div[1]/div[1]/div[2]/div/span')))
-                    adult_price = extract_number(add_adult_price.text)
-                    add_child = wait.until(EC.visibility_of_element_located((By.XPATH, '//*[@id="__next"]/main/div[2]/div/div[2]/div[2]/div/div/div[1]/div/div/div[3]/div/div[2]/div[2]/div[2]/div[3]')))
-                    add_child.click()
-                    add_child_price = wait.until(EC.visibility_of_element_located((By.XPATH, '//*[@id="__next"]/main/div[2]/div/div[2]/div[2]/div/div/div[1]/div/div/div[3]/div/div[2]/div[2]/div[1]/div[2]/div/span')))
-                    child_price = extract_number(add_child_price.text)
-                    add_infant = wait.until(EC.visibility_of_element_located((By.XPATH, '//*[@id="__next"]/main/div[2]/div/div[2]/div[2]/div/div/div[1]/div/div/div[3]/div/div[2]/div[3]/div[2]/div[3]')))
-                    add_infant.click()
-                    add_infant_price = wait.until(EC.visibility_of_element_located((By.XPATH, '//*[@id="__next"]/main/div[2]/div/div[2]/div[2]/div/div/div[1]/div/div/div[3]/div/div[2]/div[3]/div[1]/div[2]/div/span')))
-                    infant_price = extract_number(add_infant_price.text)
-
-                    book_now_button = wait.until(EC.visibility_of_element_located((By.XPATH, '//*[@id="__next"]/main/div[2]/div/div[2]/div[2]/div/div/div[1]/div/div/button')))
-                    book_now_price = extract_number(book_now_button.text)
-                    book_now_button.click()
-
-                    adult_price_2 = extract_number(wait.until(EC.visibility_of_element_located((By.XPATH,'//*[@id="__next"]/main/div[2]/div/div[2]/div[1]/div[1]/div[3]/div[1]/h2'))).text)
-                    child_price_2 = extract_number(wait.until(EC.visibility_of_element_located((By.XPATH,'//*[@id="__next"]/main/div[2]/div/div[2]/div[1]/div[1]/div[3]/div[2]/h2'))).text)
-                    infant_price_2 = extract_number(wait.until(EC.visibility_of_element_located((By.XPATH,'//*[@id="__next"]/main/div[2]/div/div[2]/div[1]/div[1]/div[3]/div[3]/h2'))).text)
-                    total_price = extract_number(wait.until(EC.visibility_of_element_located((By.XPATH,'//*[@id="__next"]/main/div[2]/div/div[2]/div[1]/div[1]/div[3]/div[4]/div[2]/h2'))).text)
-
-                    if total_price == book_now_price == infant_price + child_price + adult_price*3 == adult_price_2 + child_price_2 + infant_price_2 and starting_from_price == calendar_price == product_main_price:
-                        print(tour_name + ": Prices are correct")
-                    else:
-                        print(tour_name + ": Prices are incorrect")
-
-                    self.driver.back()
-                    self.driver.back()
-                except:
-                    pass
-            self.driver.back()
+                self.driver.back()
+            except:
+                pass
             
+
+        data = {
+            "incorrect_prices": incorrect_prices
+        }
+
+        with open("product_prices.json", "w") as json_file:
+            json.dump(data, json_file, indent=4)
 
         time.sleep(20)
         self.driver.quit()
